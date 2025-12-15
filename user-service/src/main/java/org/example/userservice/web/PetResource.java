f24392c17c29cfc07b4f3004d49714a844d17b47
Nguyễn Vĩnh Hoàng Phúc <67618897+Phuc134@users.noreply.github.com>
2025-12-15 21:19:09 +0700

    .

--- user-service/src/main/java/org/example/userservice/web/PetResource.java
+++ user-service/src/main/java/org/example/userservice/web/PetResource.java
@@ -33,8 +33,8 @@ public class PetResource {
     @PostMapping
     public Pet createPet(@RequestBody PetRequest pet) {
         Pet newPet = new Pet();
-        pet.setName(newPet.getName());
-        pet.setBirthDate(newPet.getBirthDate());
+        newPet.setName(pet.getName());
+        newPet.setBirthDate(pet.getBirthDate());
 
         PetType petType = petTypeRepository.findById(pet.getTypeId())
                 .orElseThrow(() -> new RuntimeException("PetType not found: " + pet.getTypeId()));
